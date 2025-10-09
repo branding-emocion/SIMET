6137375f5d5b5fada154e08a2b17bcbb1557087d
jhonned01 <jhonned01@gmail.com>
2025-10-09 09:49:24 -0500

    Ultimos canges

--- app/Servicios/page.js
+++ app/Servicios/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { Suspense, use, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
